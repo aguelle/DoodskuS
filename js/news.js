579e57d89b9a4460853603aab9e4d8faecf10346
Aurélien Guelle <aurelien.guelle@gmail.com>
2023-09-08 00:09:11 +0200

    add function show image in news page + upgrade CSS merch and footer

---
+++ js/news.js
@@ -0,0 +1,26 @@
+
+const articleTitle = document.querySelector('#article-title');
+const articleDescr = document.querySelector('#article-description');
+
+document.querySelectorAll('.thumbs-img').forEach(img => {
+
+document.querySelectorAll('#gallery-thumbs img[data-title]').forEach(img => {
+    img.addEventListener('click', function () {
+        document.getElementById('article-img').src = this.src;
+        articleTitle.innerText = this.dataset.title;
+        articleDescr.innerText = this.dataset.description;
+        showImage({
+            src: this.src,
+            title: this.dataset.title,
+            description: this.dataset.description
+        });
+    });
+}); 
+}); 
+
+
+function showImage(imageData) {
+    document.getElementById('article-img').src = imageData.src;
+    document.querySelector('#aritcle-title').innerText = imageData.title;
+    document.querySelector('#article-description').innerText = imageData.description;
+}
