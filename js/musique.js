cc475c5b484aaaf1ae69ff2ada49becbccb6f46b
Aurélien Guelle <aurelien.guelle@gmail.com>
2023-09-26 23:52:41 +0200

    add style to nav mobile and footer desktop

---
+++ js/musique.js
@@ -0,0 +1,11 @@
+const music = new Audio('pest adem.mp3');
+music.play();
+music.loop =true;
+music.playbackRate = 2;
+music.pause();qqazszdgfbgtyj
+
+const audioContext = new AudioContext();
+const element = document.querySelector(audio);
+const source = audioContext.createMediaElementSource(element);
+source.connect(audioContext.destination)
+audio.play();
