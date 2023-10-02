aea905771a07b3102eb1527fca9d83c6397b4ae5
Aurélien Guelle <aurelien.guelle@gmail.com>
2023-10-02 18:10:59 +0200

    add style and scipt to nav mobile

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let navMobile = document.getElementById("nav-mobile");
 let openBtn = document.getElementById("openBtn");
 let closeBtn = document.getElementById("closeBtn");
+let logoNav = document.getElementById("logo-nav");
 
 /*Add function to display Nav Mobile*/
 function openNav() {
@@ -17,3 +18,19 @@ function closeNav() {
 }
 openNav();
 closeNav();
+
+function displayNavMobileOnScroll(){
+window.addEventListener('scroll',()=>{
+  if(window.scrollY>50){
+  openBtn.classList.add('scroll')
+  }
+  else {openBtn.classList.remove('scroll')}
+});
+window.addEventListener('scroll',()=>{
+  if(window.scrollY>50){
+  logoNav.classList.add('scroll')
+  }
+  else{logoNav.classList.remove('scroll')}
+});
+};
+displayNavMobileOnScroll();
